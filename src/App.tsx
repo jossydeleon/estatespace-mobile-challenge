97b7cf9ff3093e5b2fef23311fcccd55f1241386
Josymar De Leon <josymardeleon.dev@gmail.com>
2022-05-18 09:38:34 -0400

    First commit: setting up expo app

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { registerRootComponent } from 'expo';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+const App: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <Text>EstateSpace Mobile Challenge</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+});
+
+export default registerRootComponent(App);
